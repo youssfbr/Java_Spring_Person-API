901e81e8c52c6d6ad386a9348d39d1fb1ca0c73e
Alisson Youssf <youssfbr@gmail.com>
2024-12-08 13:41:42 -0300

    Backend project created

---
+++ backend/src/main/java/com/github/youssfbr/person_api_2024/PersonApi2024Application.java
@@ -0,0 +1,13 @@
+package com.github.youssfbr.person_api_2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonApi2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonApi2024Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/github/youssfbr/person_api_2024/PersonApi2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.youssfbr.person_api_2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonApi2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
