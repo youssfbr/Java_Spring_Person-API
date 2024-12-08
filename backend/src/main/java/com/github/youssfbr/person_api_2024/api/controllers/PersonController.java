3d99cf80862d8bcfbe9ee77b47be3040a06c6429
Alisson Youssf <youssfbr@gmail.com>
2024-12-08 15:49:30 -0300

    Fix: Initial setup

--- backend/src/main/java/com/github/youssfbr/person_api_2024/api/controllers/PersonController.java
+++ backend/src/main/java/com/github/youssfbr/person_api_2024/api/controllers/PersonController.java
@@ -1,4 +1,4 @@
-package com.github.youssfbr.person_api_2024.controllers;
+package com.github.youssfbr.person_api_2024.api.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
